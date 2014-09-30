92a0ee253cdf5cfaba7c2c46730ed8b9cd7cebd9
Mohammed Davoodi <mdavoodi@vt.edu>
2014-09-30 16:51:44 -0400

    CSS like transitions now working

--- js/autotinder.js
+++ js/autotinder.js
@@ -1,8 +1,21 @@
 (function(){
   var app = angular.module('autotinder', []);
 
-  app.controller("FacebookController", function(){
-    this.review = {};
-  });
+  app.controller("CardController", ['$scope', '$timeout', '$interval', function($scope, $timeout, $interval) {
+
+   var like = function(){
+     $scope.activeIndex = $scope.cards[$scope.cards.length-1];
+     $timeout(function(){$scope.cards.unshift($scope.cards.pop());}, 500);
+   };
+
+   $scope.activeIndex = 0;
+   $scope.cards = [3, 2, 1, 6];
+
+   $scope.autoLike = function() {
+     $interval(function(){like();}, 1000);
+   };
+
+
+ }]);
 
 })();
